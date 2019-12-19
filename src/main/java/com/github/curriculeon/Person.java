7c7c8363ccc6559680874eef6ecb8eda3abab0d2
KevinW <bird831205@hotmail.com>
2019-12-19 06:58:06 -0500

    overiding .equals of person

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,7 @@
 package com.github.curriculeon;
 
+import java.util.Objects;
+
 public class Person {
 
     final private Long id;
@@ -20,5 +22,19 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
